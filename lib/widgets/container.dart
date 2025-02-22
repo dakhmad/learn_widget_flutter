9604ad3e0c92d218df8cb1e99892fe6f6695cd95
danilearner <dakhmad140104@gmail.com>
2025-02-22 21:59:50 +0700

    Widht, Height, Padding, Margin, dan only Container

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -17,14 +17,37 @@ class WidgetContainer extends StatelessWidget {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        color: Colors.blue,
-        child: Text("Hi ini adalah Container",
-        style: GoogleFonts.lato(
-          color: Colors.white,
-        ),
-        )
-      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.blue,
+            width: 100,
+            height: 100,
+            child: Text(
+              "Hi ini adalah Container",
+              style: GoogleFonts.lato(
+              color: Colors.white
+              ),
+            )
+          ),
+          Container(
+            color: Colors.lightGreen,
+            width: 100,
+            height: 100,
+            // padding: const EdgeInsets.all(10), // jarak konten yang ada didalam Container
+            // margin: const EdgeInsets.all(10), // jarak luar Container
+
+            //test padding margin
+            padding: EdgeInsets.only(left: 10, top: 9, right: 4, bottom: 8),
+            margin: EdgeInsets.only(left: 100, top: 20, right: 10, bottom: 6),
+            child: Text(
+              // "Ini adalah Container dengan parameter width, height, padding dan margin",
+              "Padding Margin",
+              style: GoogleFonts.lato(color: Colors.white),
+            ),
+          )
+        ],
+      )
     );
   }
 }
