2b4cad68d7e742bff396ceedadacb7d4cdf3cdf7
danilearner <dakhmad140104@gmail.com>
2025-02-22 21:46:45 +0700

    Mengisi container.dart dan pub add google_fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_widget_flutter/widgets/container.dart';
 import 'package:learn_widget_flutter/widgets/scaffold.dart';
 import 'package:learn_widget_flutter/widgets/state_less_ful.dart';
 
@@ -8,13 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bismillah Belajar Widget',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(
         body: Center(
-          child: FirtsScreenScaffold()
+          child: WidgetContainer()
         ),
       ),
     );
--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WidgetContainer extends StatelessWidget {
   const WidgetContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Container Widget Test",
+          style: GoogleFonts.lato(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Text("Hi ini adalah Container",
+        style: GoogleFonts.lato(
+          color: Colors.white,
+        ),
+        )
+      ),
+    );
   }
 }
