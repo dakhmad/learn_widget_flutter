fa6bf6112d626cf8c8f84939450d16164f06f6d7
danilearner <dakhmad140104@gmail.com>
2025-02-22 21:28:07 +0700

    Commit project flutter

---
+++ lib/widgets/container.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class WidgetContainer extends StatelessWidget {
+  const WidgetContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
